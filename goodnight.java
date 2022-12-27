18ced44a393258c90b09012fead7c4ae28fb9a5a
Hiroko Mastuyama <110796421+hmatsuyama3@users.noreply.github.com>
2022-12-26 21:25:37 -0500

    tried adding a goodnight popup. also trying to commit to repo from ide command line

--- goodnight.java
+++ goodnight.java
@@ -1,12 +1,16 @@
+import javax.swing.*;
+
 public class goodnight {
     public static void main (String[] args){
-        System.out.println("Goodnight world!");
 
-        try{
-            Runtime.getRuntime().exec("shutdown -s");
-        }catch(Exception e){
-            System.out.println("This was a bad idea");
-            e.printStackTrace();
-        }
+        JFrame jFrame = new JFrame();
+        JOptionPane.showMessageDialog(jFrame, "Goodnight :)");
+
+//        try{
+//            Runtime.getRuntime().exec("shutdown -s");
+//        }catch(Exception e){
+//            System.out.println("This was a bad idea");
+//            e.printStackTrace();
+//        }
     }
 }
